a53b458fb7243e8d3d35c8efdaa4de7811dca78d
Nivin Anil <nivin@kodnest.com>
2025-03-08 14:52:53 +0530

    initial commit

---
+++ src/main/resources/Day1database/day1.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE  IF NOT EXISTS `salessavvy`;
+USE `salessavvy`;
+
+// Create table 'User'
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('ADMIN','CUSTOMER') NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+)
+
